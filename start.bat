53dfe2f9f6dcf1fdeb4dc3b492c68aafb696f587
Artsiom Pashin <Lubov_Andrianova6883@rambler.ru>
2023-10-20 12:59:33 +0300

    дурка

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 title Название для консоли
-java -Xms256m -Xmx2048m -jar fabric-server-mc.1.20.1-loader.0.14.22-launcher.0.11.2.jar nogui
+java -Xms256m -Xmx6048m -jar fabric-server-mc.1.20.1-loader.0.14.22-launcher.0.11.2.jar nogui
 pause
